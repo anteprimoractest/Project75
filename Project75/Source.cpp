f2a73c886185eaaba69339d5033f1aee1e87e076
anteprimoractest <48529720+anteprimoractest@users.noreply.github.com>
2020-04-20 22:37:23 +0200

    Update Source.cpp

--- Project75/Source.cpp
+++ Project75/Source.cpp
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+
+void ispisTeksta(char * niz) {
+	for(int i = 0; i < niz.length; i++) {
+	printf("%s",niz);
+	}
+}
 
 int main() {
 	printf("Hello world!\n");
+	char niz[15] = "Novi string";
+	ispisTeksta(&niz);
 	return 0;
 }
