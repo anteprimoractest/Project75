8078b48a657fbbf142766f551f165e77a223aae1
anteprimoractest <anteprim93@gmail.com>
2020-04-20 22:53:13 +0200

    commit

--- Project75/Source.cpp
+++ Project75/Source.cpp
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 
-void ispisTeksta(char * niz) {
-	for(int i = 0; i < niz.length; i++) {
+void ispisTeksta(char *niz) {
 	printf("%s",niz);
-	}
+	char zn;
+	printf("Unesite znak: \n");
+	scanf("%c", &zn);
 }
 
 int main() {
 	printf("Hello world!\n");
-	char niz[15] = "Novi string";
-	ispisTeksta(&niz);
+	char niz[15] = "Novi string\n";
+	ispisTeksta(niz);
 	return 0;
 }
